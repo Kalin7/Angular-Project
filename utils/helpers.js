f6fbef5cd1b8eff5a30c67022645453be09a02c8
Kalin7 <103098899+Kalin7@users.noreply.github.com>
2022-04-09 14:45:42 +0300

    Update helpers.js

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 function checkFormData(data) {
     
     let errorHolder = [];
@@ -48,55 +48,4 @@ module.exports = {
     checkFormData,
     errorHandler,
     getTokenFromHeader
-=======
-function checkFormData(data) {
-    
-    let errorHolder = [];
-
-    if (data.rePass && data.rePass !== data.password) {
-        errorHolder.push({message: 'Passwords do not match'})
-    }
-    Object.entries(data).forEach(d => {
-        if (d[1] == '') {
-            errorHolder.push({message: `${d[0]} is required`});
-        }
-    });
-
-    if (errorHolder.length > 0) {
-        throw new Error(errorHolder)
-    }
-}
-
-function getTokenFromHeader(reqHeaders) {
-    let header;
-    reqHeaders.rawHeaders.forEach((h) => {
-        if (h.includes('Bearer')) {
-            header = h
-        }
-    });
-    if (!header) {
-        return '401'
-    }
-    return header.split(':')[1].split('"')[1];
-}
-
-function errorHandler(error) {
-    
-    let errorsList = [];
-    if (error.name == 'ValidationError') {
-        Object.values(error.errors).forEach(e => {
-            errorsList.push({message: e.properties.message});
-        })
-    }else {
-        errorsList.push({message: error.message});
-    }
-    
-    return errorsList;
-}
-
-module.exports = {
-    checkFormData,
-    errorHandler,
-    getTokenFromHeader
->>>>>>> d8de2de05d31f1b3fbbfdbe7dab0f736519a23e6
 }
