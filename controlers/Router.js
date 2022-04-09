79a6df10085f042efe755db7b66117e9960defb6
Kalin7 <103098899+Kalin7@users.noreply.github.com>
2022-04-09 14:40:09 +0300

    Update Router.js

--- controlers/Router.js
+++ controlers/Router.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 const router = require('express').Router();
 const userControlers = require('./User');
 const songControlers = require('./Songs');
@@ -29,38 +29,4 @@ router.put('/user/:userId/:elementType/:elementId/delete', guard.isAuthor, userC
 
 
 
-
-=======
-const router = require('express').Router();
-const userControlers = require('./User');
-const songControlers = require('./Songs');
-const articlesControlers = require('./Article');
-const postsControlers = require('./Post');
-const guard = require('../middlewear/guards');
-const { uploadS3 } = require('../service/AWS/AWS');
-
-
-router.get('/articles', articlesControlers.getArticles);
-router.get('/records', songControlers.getSongsRecords);
-router.get('/records/:id',guard.hasUser, songControlers.getSongRecordById);
-router.get('/posts/:articleId',guard.hasUser, postsControlers.getByArticleId);
-router.get('/user/:id', guard.hasUser, userControlers.getUser);
-
-router.post('/user/register', userControlers.registerAction);
-router.post('/user/login', guard.isAlreadyLoggedIn, userControlers.loginAction);
-
-router.post('/create/record', guard.hasUser, songControlers.create);
-router.post('/create/article', guard.hasUser, uploadS3.single('image'),  articlesControlers.create);
-router.post('/article/:id/update', guard.hasUser, articlesControlers.updateArticle);
-router.post('/create/post', guard.hasUser, postsControlers.create);
-
-router.put('/user/:id', guard.hasUser, userControlers.updateUserElements);
-router.put('/user/:id/info', uploadS3.single('image'), userControlers.updateUserInfo);
-router.put('/records/:id/:status', songControlers.vote);
-router.put('/user/:userId/:elementType/:elementId/delete', guard.isAuthor, userControlers.deleteElement);
-
-
-
-
->>>>>>> d8de2de05d31f1b3fbbfdbe7dab0f736519a23e6
 module.exports = router;   
