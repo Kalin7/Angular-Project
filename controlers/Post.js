6be6aab3c52ca959e2258bb2451571e43abe81c2
Kalin7 <103098899+Kalin7@users.noreply.github.com>
2022-04-09 14:39:34 +0300

    Update Post.js

--- controlers/Post.js
+++ controlers/Post.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 const { createPost, getPostsByArticleId } = require('../managers/Post');
 const { errorHandler } = require('../utils/helpers')
 
@@ -26,33 +26,4 @@ module.exports = {
             res.status(400).json(error);
         }
     }
-=======
-const { createPost, getPostsByArticleId } = require('../managers/Post');
-const { errorHandler } = require('../utils/helpers')
-
-module.exports = {
-    create: async (req, res) => {
-        const data = req.body
-
-        try {
-            const post = await createPost(data);
-            res.status(201).json(post)
-        } catch (err) {
-            const error = errorHandler(err);
-            res.status(400).json(error);
-        }
-    },
-
-    getByArticleId: async (req, res) => {
-        const id = req.params.articleId;
-        
-        try {
-            const posts = await getPostsByArticleId(id);
-            res.status(200).json(posts)
-        }catch (err) {
-            const error = errorHandler(err);
-            res.status(400).json(error);
-        }
-    }
->>>>>>> d8de2de05d31f1b3fbbfdbe7dab0f736519a23e6
 }
