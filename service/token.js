8fdc5d52f21cf52efcc0dfecef4ada54d2748c23
Kalin7 <103098899+Kalin7@users.noreply.github.com>
2022-04-09 14:45:17 +0300

    Update token.js

--- service/token.js
+++ service/token.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 require('dotenv').config();
 const jwt = require('jsonwebtoken');
 
@@ -26,28 +26,5 @@ async function verifyToken(token) {
 module.exports = {
     generateToken,
     verifyToken,
-=======
-require('dotenv').config();
-const jwt = require('jsonwebtoken');
 
-function generateToken(user) {
-    const payload = {
-        id: user._id,
-    }
-
-    return jwt.sign(
-        payload, 
-        process.env.JWT_SECRET_KEY, 
-        {expiresIn: 3600 * 24 * 60 * 60}
-    );
-}
-
-async function verifyToken(token) {
-    return jwt.verify(token, process.env.JWT_SECRET_KEY);
-}
-
-module.exports = {
-    generateToken,
-    verifyToken,
->>>>>>> d8de2de05d31f1b3fbbfdbe7dab0f736519a23e6
 };
