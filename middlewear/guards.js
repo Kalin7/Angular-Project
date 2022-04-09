832e6ffccad1dfbdad4010dce75a1bba51a30f79
Kalin7 <103098899+Kalin7@users.noreply.github.com>
2022-04-09 14:44:06 +0300

    Update guards.js

--- middlewear/guards.js
+++ middlewear/guards.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 require('dotenv').config();
 const { verifyToken } = require("../service/token");
 const { getTokenFromHeader } = require('../utils/helpers')
@@ -47,51 +47,4 @@ module.exports = {
         }
         next();
     }
-=======
-require('dotenv').config();
-const { verifyToken } = require("../service/token");
-const { getTokenFromHeader } = require('../utils/helpers')
-const {getArticleById } = require('../managers/Article');
-const {getSongById } = require('../managers/Song');
-const { getPostById } = require('../managers/Post');
-
-const elementMapper = {
-    'article': async function (id) {return await getArticleById(id)},
-    'song': async function (id) {return await getSongById(id)},
-    'post': async function (id) {return await getPostById(id)}
-}
-
-module.exports = {
-    hasUser: async (req, res, next) => {
-        const token = getTokenFromHeader(req);
-        const verified = await verifyToken(token);
-        if (!verified) {
-            return res.status(401);
-        }
-        next();        
-    },
-    
-    isAlreadyLoggedIn: (req, res, next) => {
-        
-        if (req.session.user) {
-            return res.status(400).json({message: 'User already logged in'})
-        }
-        next();
-    },
-
-    isAuthor: async (req, res, next) => {
-        
-        const elementType = req.params.elementType
-        const currentUserId = req.params.userId;
-        const elementId = req.params.elementId;      
-        const element = await elementMapper[elementType](elementId);
-        
-        if (element.author != currentUserId) {
-            if (element.author._id != currentUserId) {
-                return res.status(401).json({message: 'Unautorize'});
-            }
-        }
-        next();
-    }
->>>>>>> d8de2de05d31f1b3fbbfdbe7dab0f736519a23e6
 }
